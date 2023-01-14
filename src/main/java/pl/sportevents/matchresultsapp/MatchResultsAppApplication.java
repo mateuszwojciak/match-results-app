d1dcf70e83b8b36ebd845bdd2eb36dce64cf79a1
M W <matwojciak@gmail.com>
2023-01-14 13:17:36 +0100

    initial commit

---
+++ src/main/java/pl/sportevents/matchresultsapp/MatchResultsAppApplication.java
@@ -0,0 +1,13 @@
+package pl.sportevents.matchresultsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchResultsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchResultsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sportevents/matchresultsapp/MatchResultsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sportevents.matchresultsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchResultsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
